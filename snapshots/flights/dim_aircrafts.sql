ce6424adb991e80ad1a046de496ddc5215a237c0
Alex <belrev@yahoo.com>
2025-05-16 12:31:43 +0300

    Неделя 6

---
+++ snapshots/flights/dim_aircrafts.sql
@@ -0,0 +1,24 @@
+{% snapshot dim_aircrafts %}
+
+{{
+   config(
+       target_schema='snapshot',
+       unique_key='aircraft_code',
+       strategy='check',
+       check_cols=['model', 'range'],
+       dbt_valid_to_current = "'9999-01-01'::date",
+       snapshot_meta_column_names={
+        "dbt_valid_from": "dbt_effective_date_from",
+        "dbt_valid_to": "dbt_effective_date_to",
+        }
+   )
+}}
+
+select 
+    aircraft_code, 
+    model, 
+    "range"
+from {{ ref('stg_flights__aircraft') }}
+
+
+{% endsnapshot %}
