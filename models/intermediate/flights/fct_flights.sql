d23425b61685c2a95513141a76ff2625a44394cf
Alex <belrev@yahoo.com>
2025-05-28 23:53:02 +0300

    Неделя 16.3

--- models/intermediate/flights/fct_flights.sql
+++ models/intermediate/flights/fct_flights.sql
@@ -7,19 +7,20 @@
 select
     flight_id, 
     flight_no, 
-    scheduled_departure, 
     scheduled_arrival, 
     departure_airport, 
     arrival_airport, 
     "status", 
     aircraft_code, 
-    actual_departure, 
     actual_arrival,
-    {{concat_columns(['flight_id', 'flight_no'])}} as flight_info
+    actual_departure,
+    scheduled_departure, 
+    case 
+        when actual_departure is not null and scheduled_departure < actual_departure then actual_departure - scheduled_departure
+        else INTERVAL '0 seconds'
+    end  as flight_departure_delay,
+    case 
+        when actual_departure is not null and actual_arrival is not null then actual_arrival - actual_departure
+        else INTERVAL '0 seconds'
+    end  as actual_duration_flight
 from {{ ref('stg_flights__flights') }}
-
-{% set unique_status = dbt_utils.get_column_values(table=this, column='status') %}
-
-{% for status in unique_status %}
-    {{ log(status, True) }}
-{% endfor %}
--- models/staging/flights/stg_flights__bookings_append.sql
+++ models/staging/flights/stg_flights__bookings_append.sql
@@ -7,11 +7,12 @@
 }}
 
 select 
-    {{ bookref_to_bigint("book_ref") }} as book_ref, 
+    book_ref, 
     book_date, 
     {{kop_to_ruble("total_amount", 2)}} as total_amount
 from {{ source("demo_src", "bookings") }}
 
 {%- if is_incremental() %}
-where {{ bookref_to_bigint("book_ref") }} > (select MAX({{ bookref_to_bigint("book_ref") }}) from {{this}})
+WHERE
+    {{ bookref_to_bigint('book_ref') }} > (SELECT MAX({{ bookref_to_bigint('book_ref') }}) FROM {{ this }})
 {% endif %}
